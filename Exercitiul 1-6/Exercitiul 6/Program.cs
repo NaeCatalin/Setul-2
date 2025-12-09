0c7f6b7c01cf6e599dc12ae213a79d154a2e77f2
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:26:26 +0200

    Add files via upload

---
+++ Exercitiul 1-6/Exercitiul 6/Program.cs
@@ -0,0 +1,30 @@
+﻿//Se da o secventa de n numere.
+//Sa se determine daca numerele din secventa sunt in ordine crescatoare. 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        bool crescator = true;
+
+        int anterior = int.Parse(Console.ReadLine()); //Citim primul nr
+
+        for (int i = 0; i < n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            if (x == anterior)
+                crescator = false;
+
+            anterior = x; //Actualizam ultimu nr
+        }
+        if (crescator)
+            Console.WriteLine("DA");
+        else
+            Console.WriteLine("NU");
+    }
+}
