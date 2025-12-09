405eb683052473c01debc6a50e43e904cf5c50b1
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:28:56 +0200

    Add files via upload

---
+++ Exercitiul 1-11/Exercitiul 11/Program.cs
@@ -0,0 +1,33 @@
+﻿//Se da o secventa de n numere.
+//Se cere sa se caculeze suma inverselor acestor numere. 
+
+using System;
+using System.Net.NetworkInformation;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        double sum = 0;
+
+        for (int i =1; i <= n; i ++)
+        {
+            Console.WriteLine("Numarul " + i + ":" );
+            double x = double.Parse(Console.ReadLine());
+            // evitam impartirea la 0
+            if (x !=0)
+            {
+                sum += 1 / x;
+            }
+            else
+            {
+                Console.WriteLine("Impartirea la 0 nu este permisa, numarul va fi ignorat.");
+            }
+            Console.WriteLine("Suma inverselor este: " + sum);
+        }
+
+    }
+}
