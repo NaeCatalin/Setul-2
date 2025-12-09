27f389979e94475ef323fdb7af747a7c4e277178
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:21:49 +0200

    Add files via upload

---
+++ Exercitiul 1-1/Exercitiul 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int countEven = 0;
+        for (int i = 0; i< n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x % 2 == 0)
+            {
+                countEven++;
+         
+           }
+            Console.WriteLine(countEven);
+        }
+
+    }
+}
