779fd3720a435209b58378fe0e5bad3106611017
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:22:51 +0200

    Add files via upload

---
+++ Exercitiul 1-3/Exercitiul 3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Calculati suma si produsul numerelor de la 1 la n. 
+using System;
+
+class Program
+{
+    static void Main ()
+    {
+        int n = int.Parse(Console.ReadLine());
+        long suma = 0;
+        long produs = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            suma += i;        //adunam nr
+                produs *= i;    //inmultim nr
+        }
+        Console.WriteLine("Suma: " + suma);
+        Console.WriteLine("Produs: " + produs);
+    }
+}
