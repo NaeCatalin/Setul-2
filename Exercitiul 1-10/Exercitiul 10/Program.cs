2c152bbdd4a7a34e3f0d67988966c9f7e996c591
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:28:14 +0200

    Add files via upload

---
+++ Exercitiul 1-10/Exercitiul 10/Program.cs
@@ -0,0 +1,43 @@
+﻿//Se da o secventa de n numere.
+//Care este numarul maxim de numere consecutive egale din secventa. 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("n=");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Primul numar:");
+        int prev = int.Parse(Console.ReadLine());
+
+        int currentCount = 1; //lungimea curenta de elemente egale
+                int maxCount = 1; //lungimea maxima de elemente egale
+
+        for (int i = 2; i < n; i++)
+        {
+            Console.WriteLine("Urmatorul numar:");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x==prev)
+            {
+                currentCount++;
+            }
+            else
+            {
+                if (currentCount > maxCount)
+                {
+                    maxCount = currentCount;
+                }
+                currentCount = 1; //resetam lungimea curenta
+            }
+            prev = x;
+        }
+        //Verificam la final
+        if (currentCount > maxCount) 
+        maxCount = currentCount;
+        Console.WriteLine("Numarul maxim de numere consecutive egale este: " + maxCount);
+    }
+}
