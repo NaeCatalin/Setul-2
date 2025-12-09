65ba1ddf828b8f7cae2506f20abdb0449887db91
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:31:19 +0200

    Add files via upload

---
+++ Exercitiul 1-16/Exercitiul 16/Program.cs
@@ -0,0 +1,76 @@
+﻿    //O <secventa bitonica rotita> este o secventa bitonica sau una ca poate fi
+    //transformata intr-o secventa bitonica prin rotiri succesive (rotire = primul
+    //element devine ultimul).
+    //Se da o secventa de n numere. Se cere sa se determine daca este o secventa
+    //bitonica rotita.
+    
+
+
+using System;
+using System.ComponentModel.Design;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("n = ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Primul numar = ");
+        int firstNumber = int.Parse(Console.ReadLine());
+        int previousNumber = firstNumber;
+
+        int schimbari = 0;
+        int directii = 0; // 0 = necunoscut, 1 = crescator, -1 = descrescator
+
+        for (int i = 2; i < n; i++)
+        {
+            Console.WriteLine("Numarul" + i + ":");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x > previousNumber)
+            {
+                if (directii == -1)
+                {
+                    schimbari++;
+                    directii = 1;
+                }
+                else if (directii == 0)
+                {
+                    directii = 1;
+                }
+
+            }
+
+            else if (x < previousNumber)
+            {
+                if (directii == 1)
+                {
+                    schimbari++;
+                    directii = -1;
+                }
+                else if (directii == 0)
+                {
+                    directii = -1;
+                }
+                previousNumber = x;
+            }
+            if (firstNumber == previousNumber)
+            {
+                if (directii == 1) { schimbari++; }
+
+            }
+            else if (firstNumber == previousNumber)
+                {
+                if (directii == 1) schimbari++;
+                }
+            if (schimbari == 2)
+            
+                Console.WriteLine("Secventa este bitonica rotita");
+            else
+                Console.WriteLine("Secventa nu este bitonica rotita");
+
+        }
+    }
+}
+
