4f3ffb05f8c7bc14f3f30a60bdbe837d6dba4d7a
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:26:47 +0200

    Add files via upload

---
+++ Exercitiul 1-7/Exercitiul 7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main ()
+    {
+        int n =int.Parse(Console.ReadLine());
+
+        int x = int .Parse(Console.ReadLine());  //citim primul nr
+        int minim = x;
+        int maxim = x;
+
+        for (int i = 1; i<n; i++)
+        {
+            x = int.Parse(Console.ReadLine());
+
+            if (x < minim)
+                minim = x;
+            if (x > maxim)
+                maxim = x;
+        }
+        Console.WriteLine("Minimul este: " + minim);
+        Console.WriteLine("Maximul este: " + maxim);
+    }
+}
