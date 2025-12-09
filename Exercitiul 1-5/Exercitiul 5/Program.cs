2e20c11afd3ee1dbb77f524fccbf5c21fcf2fc6e
NaeCatalin <naecatalinvasile@gmail.com>
2025-12-09 15:24:59 +0200

    Add files via upload

---
+++ Exercitiul 1-5/Exercitiul 5/Program.cs
@@ -0,0 +1,24 @@
+﻿//Cate elemente dintr-o secventa de n numere sunt egale
+//cu pozitia pe care apar in secventa.
+//Se considera ca primul element din secventa este pe pozitia 0. 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int count = 0;
+        for (int i = 0; i < n; i++)
+        {
+            int x = int.Parse(Console.ReadLine());
+            if (x == i)
+                count++;
+
+        }
+        Console.WriteLine(count);
+        {
+        }
+    }
+}
